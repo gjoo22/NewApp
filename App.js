6456d97627a7c45a2cf049b044d5bfdb05568efd
gjoo22 <87410616+gjoo22@users.noreply.github.com>
2021-07-14 16:52:13 +0900

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Your Health Care Partner</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#7b91b5',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
